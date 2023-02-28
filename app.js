4c325ac987e85541935fa45f725717b0cf391973
nick <chenhsuanfu@gmail.com>
2023-02-28 10:10:40 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-// 載入 mongoose
-const mongoose = require('mongoose')
+
 // 載入 express-handlebars
 const exphbs = require('express-handlebars')
 // 引用 body-parser
@@ -10,31 +9,11 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 // 引用路由器
 const routes = require('./routes')
-
+// 引用mongoose
+require('./config/mongoose')
 // 定義連接 port number
 const port = 3000
-
-
-
-// 加入這段 code, 僅在非正式環境時, 使用 dotenv
-if (process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
-}
-
 const app = express()
-// 設定連線到mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-    console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-    console.log('mongodb connected!')
-})
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 // 設定express靜態檔案位置
